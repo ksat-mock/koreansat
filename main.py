65554b05761a8af303daa44394a65c210646f42e
ksat-mock <chicwindist@gmail.com>
2025-01-18 18:34:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -357,18 +357,18 @@ def second_page():
                         #     key=sub_key
                         # )
 
-                        # 고유한 key 생성
-                        sub_key = f"sub_question_{st.session_state.current_tab}_{idx}_{sub_idx}"
+                        # # 고유한 key 생성
+                        # sub_key = f"sub_question_{st.session_state.current_tab}_{idx}_{sub_idx}"
                         
-                        # 슬라이더로 1 ~ 5 사이 점수 선택
-                        selected_sub = st.slider(
-                            f"{idx + 1}-{sub_idx + 1}의 답을 선택하세요:",
-                            min_value=1,  # 최소 값
-                            max_value=5,  # 최대 값
-                            value=3,  # 기본값 (중간 값으로 설정)
-                            step=1,  # 1단위로 증가
-                            key=sub_key
-                        )
+                        # # 슬라이더로 1 ~ 5 사이 점수 선택
+                        # selected_sub = st.slider(
+                        #     f"{idx + 1}-{sub_idx + 1}의 답을 선택하세요:",
+                        #     min_value=1,  # 최소 값
+                        #     max_value=5,  # 최대 값
+                        #     value=3,  # 기본값 (중간 값으로 설정)
+                        #     step=1,  # 1단위로 증가
+                        #     key=sub_key
+                        # )
 
             
                         # # 하위 문제 답안 저장
@@ -377,6 +377,19 @@ def second_page():
                         # st.session_state[tab_key][f"{idx}-sub"][f"{sub_idx}"] = (
                         #     sub_q["choices"].index(selected_sub) if selected_sub else None
                         # )
+
+                        # 고유한 key 생성 및 슬라이더로 점수 선택
+                        for problems_q_key, sub_answers in st.session_state[problems_key].items():
+                            for sub_idx, _ in enumerate(sub_answers):
+                                sub_key = f"sub_question_{problems_q_key}_sub{sub_idx+1}"
+                                st.session_state[problems_key][problems_q_key][sub_idx] = st.slider(
+                                    f"문제 평가 {problems_q_key} - {sub_idx + 1}의 답을 선택하세요:",
+                                    min_value=1,  # 최소 값
+                                    max_value=5,  # 최대 값
+                                    value=3,  # 기본값 (중간 값으로 설정)
+                                    step=1,  # 1단위로 증가
+                                    key=sub_key
+                                )
 
                         
                         # 문제 평가 답안 저장
