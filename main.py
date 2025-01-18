67f1d0fa7c11b04cefda22faa2ea759a8e025524
ksat-mock <chicwindist@gmail.com>
2025-01-18 10:46:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ def first_page():
 
 # 두 번째 페이지: 문제 풀이 페이지
 def second_page():
+    st.set_page_config(layout="wide")
     st.markdown(
         """
         <style>
@@ -75,7 +76,6 @@ def second_page():
         """,
         unsafe_allow_html=True
     )
-    st.set_page_config(layout="wide")
 
     tabs_data = tabs_data = get_data()
 
