2efd584b3447c601e04900a952fdb38d172e5c1c
ksat-mock <chicwindist@gmail.com>
2025-01-20 00:31:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,6 @@ def get_data():
             "choices": choices,
             "sub_questions_passage": sub_questions_passage,  # 하위 질문 추가 - 지문
             "sub_questions_problems": sub_questions_problems  # 하위 질문 추가 - 문제
-            # "sub_sub_questions_passage": sub_sub_questions_passage,
-            # "sub_sub_questions_problems": sub_sub_questions_problems
         })
 
     
@@ -270,10 +268,14 @@ def second_page():
                         for problems_q_key, sub_answers in st.session_state[problems_key].items():
                             # for sub_idx, _ in enumerate(sub_answers):
                             sub_key = f"sub_question_{idx + 1}_{problems_q_key}_sub{sub_idx+1}"
+                            if not math.isnan(sub_sub["sub_sub_questions_problems"][sub_idx]):
+                                sub_question = sub_sub["sub_sub_questions_problems"][sub_idx]
+                            else:
+                                sub_question = " "
                             st.session_state[problems_key][problems_q_key][sub_idx] = st.radio(
                                 # f"문제 평가 {problems_q_key} - {sub_idx + 1}의 답을 선택하세요:",
                                 # f"{tabs_data[tabs[0]]["questions"]["sub_sub_questions_problems"][sub_idx]}",
-                                f"{sub_sub["sub_sub_questions_problems"][sub_idx]}",
+                                f"({sub_question})",
                                 options=[1, 2, 3, 4, 5],  # 1부터 5까지 선택 가능
                                 index=None,  # 기본값 없음
                                 key=sub_key
