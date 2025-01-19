d0454c3b431a16fbf2f8d9e1a6fee7132402881e
ksat-mock <chicwindist@gmail.com>
2025-01-20 00:35:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -269,13 +269,13 @@ def second_page():
                             # for sub_idx, _ in enumerate(sub_answers):
                             sub_key = f"sub_question_{idx + 1}_{problems_q_key}_sub{sub_idx+1}"
                             if sub_sub["sub_sub_questions_problems"][sub_idx] is not None:
-                                sub_question = sub_sub["sub_sub_questions_problems"][sub_idx]
+                                sub_question = f"({sub_sub["sub_sub_questions_problems"][sub_idx]})"
                             else:
                                 sub_question = " "
                             st.session_state[problems_key][problems_q_key][sub_idx] = st.radio(
                                 # f"문제 평가 {problems_q_key} - {sub_idx + 1}의 답을 선택하세요:",
                                 # f"{tabs_data[tabs[0]]["questions"]["sub_sub_questions_problems"][sub_idx]}",
-                                f"({sub_question})",
+                                sub_question,
                                 options=[1, 2, 3, 4, 5],  # 1부터 5까지 선택 가능
                                 index=None,  # 기본값 없음
                                 key=sub_key
